5484ab2f461bc3f51909fd993f19ba4e4f7a52a2
Arthur Albarelli <arthuralbarelli@gmail.com>
2026-09-01 17:01:02 -0300

    Create Text Area

--- src/nbvim/main.py
+++ src/nbvim/main.py
@@ -1,8 +1,10 @@
-from textual.app import App
-
+from textual.app import App, ComposeResult
+from textual.widgets import TextArea
 
 class NbVim(App):
-    pass
+    
+    def compose(self) -> ComposeResult:
+        yield TextArea.code_editor(language="python")
 
 if __name__ == "__main__":
     app = NbVim()
